0f7da413d735f183d47af5c7ddbcc3b9da5bd662
@programmingst <lucky.a.acidera@gmail.com>
2025-06-23 00:52:06 +0800

    Frontend

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary text-center">Bootstrap is Working!</h1>
+      <button className="btn btn-danger">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
