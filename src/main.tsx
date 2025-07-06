21655bcbc906298d9930c2a4e74f2445c683c588
@programmingst <lucky.a.acidera@gmail.com>
2025-07-06 15:33:31 +0800

    Task Table

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
